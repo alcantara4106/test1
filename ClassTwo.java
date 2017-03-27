a0b31292f7b818079320dfc152aa26f26b0026ad
alcantara4106 <alcantara4106@mydusd.org>
2017-03-27 11:09:48 -0700

    commit test

---
+++ ClassTwo.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class ClassTwo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClassTwo extends ClassOne
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ClassTwo
+     */
+    public ClassTwo()
+    {
+        // initialise instance variables
+        x = 5;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int methodTwo(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
